48176f61cca8ad9595876a0c5b870ce8ffe8841f
Jakub Dobrowolski <kuba.dobry-wp.pl@wp.pl>
2023-11-06 16:03:39 +0100

    Initial commit

---
+++ src/components/HomeImg.js
@@ -0,0 +1,27 @@
+import "./HomeImgStyles.css";
+
+import React from 'react'
+
+import BgImg from "../assets/bg.jpg"
+import { Link } from "react-router-dom";
+
+const HomeImg = () => {
+  return (
+    <div className="section">
+        <div className="mask">
+            <img className="bg-img" src={BgImg} alt="Background-Img"/>
+        </div>
+        <div className="content">
+            <p>You have the pleasure of seeing my portfolio</p>
+            <h1>Hello, My name is Jakub Dobrowolski</h1>
+            <h1>Django & React Developer</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn btn-light">Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default HomeImg
---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import HomeImg from '../components/HomeImg'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <HomeImg/>
+    </div>
+  )
+}
+
+export default Home
